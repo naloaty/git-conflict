4ef858f09936510999d3349b1bc8c7e560fe2b8a
Naloaty <53543446+Naloaty@users.noreply.github.com>
2023-05-18 19:24:06 +0500

    Add address field

--- Person.py
+++ Person.py
@@ -1,9 +1,10 @@
 class Person:
     """Simple class that represents a person"""
 
-    def __init__(self, name: str, age: int):
+    def __init__(self, name, age, address):
         self.name = name
         self.age = age
+        self.address = address
 
     def __str__(self):
-        return f"Person(name={self.name}, age={self.age})"
+        return f"Person(name={self.name}, age={self.age}, address={self.address})"
