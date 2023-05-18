26a1a07af29482cbcb8b9dca16c041d85acf10b8
Naloaty <53543446+Naloaty@users.noreply.github.com>
2023-05-18 19:22:54 +0500

    Add typing & comment

--- Person.py
+++ Person.py
@@ -1,6 +1,7 @@
 class Person:
+    """Simple class that represents a person"""
 
-    def __init__(self, name, age):
+    def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
 
